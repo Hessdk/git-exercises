bedf5f3fb7cba7c6ee5b3235b2ba1865e3d2b2bb
oskar123456 <xsc176@alumni.ku.dk>
2024-02-01 11:48:21 +0100

    Exercise 2 - Commit 2

--- git-exercises/src/main/java/org/example/Main.java
+++ git-exercises/src/main/java/org/example/Main.java
@@ -5,9 +5,9 @@ public class Main {
         for (int i = 1; i <  num; i++){
             String str = "";
             if (i % 3 == 0)
-                str += "Fizz";
-            if (i % 5 == 0)
                 str += "Buzz";
+            if (i % 5 == 0)
+                str += "Fizz";
             str = (str == "") ? Integer.toString(i) : str;
             str = (i < num - 1) ? str + ", " : str;
             System.out.print(str);
