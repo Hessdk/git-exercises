ff187a1286816b0c85bcb2a508bcb08b32414568
Hessdk <113091971+Hessdk@users.noreply.github.com>
2024-02-01 10:30:57 +0100

    Fizz and Buzz from jacob

--- git-exercises/src/main/java/org/example/Main.java
+++ git-exercises/src/main/java/org/example/Main.java
@@ -3,7 +3,31 @@ package org.example;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        
+        Main main = new Main();
+        main.writeNumbers();
+
+    }
+
+    public int writeNumbers() {
+
+
+        for (int i = 1; i <= 100; i++) {
+            boolean checker = false;
+            if (i % 3 == 0 && i % 5 == 0) {
+                checker = true;
+                System.out.println("FizzBuzz");
+            } else if (i % 3 == 0) {
+                checker = true;
+                System.out.println("Fizz");
+            } else if (i % 5 == 0) {
+                checker = true;
+                System.out.println("Buzz");
+            }
+            if (!checker) {
+                System.out.println(i);
+            }
+
+        }
+        return 1;
     }
 }
